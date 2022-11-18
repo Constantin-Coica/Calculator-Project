8fa528c5fab424adc853dada74f2af809c1b197f
Constantin-Coica <constantin_marian.coica@yahoo.com>
2022-11-18 15:47:09 +0200

    Completed Functionality

--- separeteFunctionsForTesting.js
+++ separeteFunctionsForTesting.js
@@ -33,14 +33,49 @@ export const addCommas = (number) =>{
     return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
 }
 
+export const doPercentage =(number) =>{
+    return Number(number)/100;
+
+}
+
+export const changeSign = (number) =>{
+    let result = 0;
+    if(Number(number) > 0){
+        result = -number;
+    }
+
+    if(Number(number)< 0){
+        result = -(number);
+    }
+
+    return result;
+     
+}
+
+//Tests for doMathematics function:
 console.log(doMathematics([1,"+",2]));
 console.log(doMathematics([1,"-",2]));
 console.log(doMathematics([1,"/",0]));
 console.log(doMathematics([1,"*",5]));
 
+
+//Tests for addCommas function:
 console.log(addCommas(1234));
 console.log(addCommas(12345));
 console.log(addCommas(123456));
 console.log(addCommas(1234567));
+
+//Tests for doPercentage function:
+console.log(doPercentage(50));
+console.log(doPercentage("50"));
+console.log(doPercentage(0.5));
+
+
+//Tests for changeSign function:
+console.log(changeSign(12));
+console.log(changeSign(-12));
+console.log(changeSign(0));
+console.log(changeSign("12"));
+console.log(changeSign("-12"));
 
 
