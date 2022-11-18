0559ba012450544a49d2eafe608bdaf5df554b16
Constantin-Coica <constantin_marian.coica@yahoo.com>
2022-11-18 15:46:44 +0200

    Completed Functionality

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { doMathematics,addCommas } from "./separeteFunctionsForTesting.js";
+import { doMathematics,addCommas, doPercentage, changeSign } from "./separeteFunctionsForTesting.js";
 
 const display = document.querySelector(".container__display");
 const deleteBtn = document.querySelector(".button.delete");
@@ -16,6 +16,8 @@ const addition = document.querySelector(".button.addition");
 const equalsBtn = document.querySelector(".button.equals");
 
 let toOperateOn = [];
+let activeOperand = false;
+let activeDot = false;
 
 const operators = {
     "+":function(a,b){
@@ -37,8 +39,10 @@ const operators = {
 
 numbers.forEach((element) =>{
     element.addEventListener("click", (event) =>{
-        if(display.innerHTML == 0){
+        if(display.innerHTML == 0 || activeOperand == true){
         display.innerHTML = event.target.innerHTML;
+        activeOperand = false;
+        activeDot = false;
         } else {
             display.innerHTML += event.target.innerHTML;
         }
@@ -47,16 +51,75 @@ numbers.forEach((element) =>{
 });
 
 addition.addEventListener("click", ()=>{
+
     toOperateOn.push(display.innerHTML);
     toOperateOn.push("+");
+    activeOperand = true;
+    deleteBtn.innerHTML = "C";
 
-})
+});
 
 equalsBtn.addEventListener("click", ()=>{
     toOperateOn.push(display.innerHTML);
     display.innerHTML=doMathematics(toOperateOn);
     toOperateOn = [];
+    deleteBtn.innerHTML = "AC"
+});
+
+substraction.addEventListener("click", ()=>{
+    toOperateOn.push(display.innerHTML);
+    toOperateOn.push("-");
+    activeOperand=true;
+});
+
+multiplication.addEventListener("click", ()=>{
+    toOperateOn.push(display.innerHTML);
+    toOperateOn.push("*");
+    activeOperand=true;
 })
+
+divisionBtn.addEventListener("click", () =>{
+    toOperateOn.push(display.innerHTML);
+    toOperateOn.push("/");
+    activeOperand=true;
+});
+
+percentage.addEventListener("click", () =>{
+    display.innerHTML = doPercentage(display.innerHTML);
+});
+
+sign.addEventListener("click", () =>{
+    display.innerHTML = changeSign(display.innerHTML);
+})
+
+deleteBtn.addEventListener("click", () =>{
+    if(deleteBtn.innerHTML == "C"){
+    display.innerHTML = 0;
+    activeDot = false;
+    deleteBtn.innerHTML = "AC";
+    }else {
+        display.innerHTML = 0;
+        toOperateOn = [];
+    }
+
+    
+});
+
+zero.addEventListener("click", () =>{
+    if(display.innerHTML != 0 && toOperateOn.length != 2){
+        display.innerHTML += 0;
+    } else {
+        display.innerHTML = 0;
+    }
+});
+
+dotBtn.addEventListener("click", ()=>{
+    if(!activeDot){
+        activeDot = true;
+        display.innerHTML += ".";
+    }
+});
+
 
 
 
